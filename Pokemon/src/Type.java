03dfa3adc6400358b666e6a31115ac59852a9936
4804164317 <4804164317@HFR-3CZ8H.ocps.k12.fl.us>
2016-04-30 16:06:44 -0400

    Type

---
+++ Pokemon/src/Type.java
@@ -0,0 +1,25 @@
+
+public class Type 
+{
+	String type;
+	double S = 1.5;
+	double R = 1.25;
+	
+	//grass
+	Type(String nType)
+	{
+			type = nType;
+	}
+	
+	public String getType()
+	{
+		return type;
+	}
+	
+	public void setType(String nType)
+	{
+		type = nType;
+	}
+	
+	
+}
